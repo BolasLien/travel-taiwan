cc676cc5f240306f4182e8cf15243ea55e5e8873
Bolas <ltzuen@gmail.com>
2021-11-11 13:32:26 +0800

    [mod] 搜尋類別html調整

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <section class="search-group container">
-    <div class="row mb-5"></div>
+    <!-- <div class="row mb-5"></div> -->
     <div class="row pt-5 mb-2">
       <h1 class="">景點</h1>
     </div>
-    <div class="category row mb-3">
-      <div class="col-sm-1">
-        <button type="button" class="btn btn-primary">景點</button>
-      </div>
-      <div class="col-sm-1">
-        <button type="button" class="btn btn-primary">餐飲</button>
-      </div>
-      <div class="col-sm-1">
-        <button type="button" class="btn btn-primary">旅宿</button>
-      </div>
-      <div class="col-sm-1">
-        <button type="button" class="btn btn-primary">活動</button>
-      </div>
-    </div>
+      <ul class="nav nav-pills row mb-3">
+        <li class="nav-item col-sm-1">
+          <a class="nav-link active" aria-current="page" href="#">景點</a>
+        </li>
+        <li class="nav-item col-sm-1">
+          <a class="nav-link" href="#">餐飲</a>
+        </li>
+        <li class="nav-item col-sm-1">
+          <a class="nav-link" href="#">旅宿</a>
+        </li>
+        <li class="nav-item col-sm-1">
+          <a class="nav-link">活動</a>
+        </li>
+      </ul>
     <form class="row gx-3 gy-2 align-items-center pb-3">
       <div class="col-sm-3">
         <label class="visually-hidden" for="specificSizeSelect">選擇地區</label>
@@ -59,16 +59,29 @@
               Some quick example text to build on the card title and make up the bulk of the card's content.
             </p>
             <div class="tag">
-              <span class="badge bg-secondary">New</span>&nbsp;
-              <span class="badge bg-secondary">New</span>&nbsp;
-              <span class="badge bg-secondary">New</span>&nbsp;
-              <span class="badge bg-secondary">New</span>&nbsp;
+              <span class="badge bg-secondary">New</span>&nbsp; <span class="badge bg-secondary">New</span>&nbsp;
+              <span class="badge bg-secondary">New</span>&nbsp; <span class="badge bg-secondary">New</span>&nbsp;
               <span class="badge bg-secondary">New</span>&nbsp;
             </div>
           </div>
         </div>
       </li>
     </ul>
+    <div class="row">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </section>
 </template>
 
@@ -84,15 +97,14 @@ const todolist = [
   {num: '1', title: '3534'},
   {num: '1', title: '3534'},
 ]
-
 </script>
 
 <style>
 * {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    font-family: 'Noto Sans TC', 'Microsoft JhengHei', sans-serif;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-family: 'Noto Sans TC', 'Microsoft JhengHei', sans-serif;
 }
 .search-group {
   background: url('../assets/景點banner.png') center;
