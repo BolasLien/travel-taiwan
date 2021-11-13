cdbda806e7afafc68c22cf1c280f80d6ffcb5722
Bolas <ltzuen@gmail.com>
2021-11-13 23:20:35 +0800

    下滑時導覽列效果

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,37 @@
 <template>
-  <nav class="navbar fixed-top navbar-light navbar-expand-lg">
+  <nav class="navbar fixed-top navbar-light navbar-expand-lg" :class="cssSticky">
     <div class="container">
-      <a class="navbar-brand" href="#"><img src="../assets/icon_logo.svg" alt=""></a>
+      <a class="navbar-brand" href="#">
+        <img :src="imgLogo" alt="" />
+      </a>
     </div>
   </nav>
 </template>
+
+<script setup>
+import logo from '../assets/icon_logo.svg'
+import logo2 from '../assets/icon_logo_2.svg'
+import {onMounted, ref} from 'vue'
+
+let cssSticky = ref('')
+let imgLogo = ref(logo)
+
+onMounted(() => {
+  window.onscroll = function () {
+    checkSticky()
+  }
+
+  let navbar = document.querySelector('.navbar')
+  let sticky = navbar.offsetTop
+
+  const checkSticky = () => {
+    if (window.pageYOffset > sticky) {
+      cssSticky.value = 'sticky'
+      imgLogo.value = logo2
+    } else {
+      cssSticky.value = ''
+      imgLogo.value = logo
+    }
+  }
+})
+</script>
